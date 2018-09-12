ce1b4b8c76ded05d5a40e4ed857e1b530926c87d
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-12 14:32:06 +0200

    Change js files

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,25 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
-    }
+    },
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader'},
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					}
+				]
+			}
+		]
+	}	
 };
